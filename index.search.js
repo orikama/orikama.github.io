247189dd51271a8f8b8ac52f92eaf315fb530a9c
orikama <orikama@users.noreply.github.com>
2023-01-29 16:51:01 +0000

    deploy: cab2be6796b55050e53974adb6f126a9c6a9dd42

---
+++ index.search.js
@@ -0,0 +1,2 @@
+var relearn_search_index=[{content:"",description:"",tags:null,title:"Categories",uri:"/categories/index.html"},{content:`Home
+`,description:"",tags:null,title:"Home",uri:"/index.html"},{content:"",description:"",tags:null,title:"Tags",uri:"/tags/index.html"}]
